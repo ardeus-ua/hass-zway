9992fda2cebb38a7805b5210ab5255e79007f0a2
ardeus-ua <ardeus@gmail.com>
2018-12-04 15:33:31 +0200

    Update zway.py

--- climate/zway.py
+++ climate/zway.py
@@ -10,7 +10,7 @@ from homeassistant.const import CONF_NAME, CONF_URL, CONF_USERNAME, CONF_PASSWOR
     ATTR_UNIT_OF_MEASUREMENT, ATTR_TEMPERATURE
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['pyzway==0.2.0']
+REQUIREMENTS = ['pyzway==0.3.0']
 
 _LOGGER = logging.getLogger(__name__)
 
