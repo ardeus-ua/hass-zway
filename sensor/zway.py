2ba2bd01fc52de9a100b8b15be2cec50aa6731c1
ardeus-ua <ardeus@gmail.com>
2018-12-05 19:52:46 +0200

    Update zway.py

--- sensor/zway.py
+++ sensor/zway.py
@@ -12,7 +12,7 @@ from homeassistant.helpers.icon import icon_for_battery_level
 
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['https://github.com/ardeus-ua/pyzway/archive/v0.3.0.zip#pyzway==0.3.0']
+REQUIREMENTS = ['https://github.com/ardeus-ua/pyzway/archive/master.zip#pyzway==0.3.0']
 
 SENSOR_TYPES = {
     'battery': ['Battery Level', '%'],
